c9c6eac9c9b1a51f810d103b9e873f1544a98c60
Rafael Lucas <arquivoweyne@gmail.com>
2020-03-31 17:49:24 -0300

    adicionando musicas

--- data.js
+++ data.js
@@ -2,31 +2,31 @@ export default [
   {
     title:
       "Ele podia se ver tão perto",
-    artist: "Lemon",
+    artist: "U2 - Lemon",
     cover: "01.jpg",
     file: "01.mp3"
   },
   {
     title: "Eu vou estar com você novamente",
-    artist: "New Years Day",
+    artist: "U2 - New Years Day",
     cover: "02.jpg",
     file: "02.mp3"
   },
   {
     title: "Desaparecer sem deixar pista",
-    artist: "Where the streets have no name",
+    artist: "U2 - Where the streets have no name",
     cover: "03.jpg",
     file: "03.mp3"
   },
   {
     title: "Você poderia se mover neste momento",
-    artist: "Mysterious Ways",
+    artist: "U2 - Mysterious Ways",
     cover: "04.jpg",
     file: "04.mp3"
   },
   {
-    title: "Júnior Pleno ou Sênior, qual a diferença?",
-    artist: "Diego Fernandes",
+    title: "Minha cabeça está em algum lugar no caminho",
+    artist: "U2 - Even Better Than The Real Thing",
     cover: "05.jpg",
     file: "05.mp3"
   }
